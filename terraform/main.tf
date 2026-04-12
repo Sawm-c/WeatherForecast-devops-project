c0c7add76d4a81eaec8443e7ea0eb77aad476b00
Sawm-c <tanh3489@gmail.com>
2026-04-12 17:36:09 +0700

    infra: update docker-compose installation method in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ sudo systemctl enable docker
 
 sudo usermod -aG docker ec2-user
 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo yum install docker-compose-plugin -y 
 sudo chmod +x /usr/local/bin/docker-compose
 EOF
 }
